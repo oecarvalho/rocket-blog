fe80a4f45e64bea154953e6700411ca8573a0d9e
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2026-01-27 22:00:47 -0300

    adicionando section suport

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { FeatureSection } from "@/components/feature-section/feature-section";
 import { HeroSection } from "@/components/hero-section/hero-section";
+import { SuportSection } from "@/components/suport-section/suport-section";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <article className="flex flex-col">
         <HeroSection/>
         <FeatureSection/>
+        <SuportSection/>
       </article>
     </>
   )
